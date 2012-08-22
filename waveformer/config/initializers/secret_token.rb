3183ec9ee52d701bb89b279fa96dc6fef772d383
Nicolai Seerup <nicolai@music-in-motion.co.uk>
2012-08-22 13:12:07 +0200

    h

---
+++ waveformer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Waveformer::Application.config.secret_token = '2177c77f62fad030f60bed272369f7fd7ec5c7614bd077e33391fd987d3526a3e6e0af055297b2b3ddcfab1a18dec84ae824171d8a4159021b0813a333c619c6'
